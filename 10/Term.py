d29694c85cc9c893cb325dbad30e2db97e7afddc
Itai Bentolila <itai.bentolila@apple.com>
2025-01-09 21:34:55 +0200

    added consts for test discovery

--- 10/Term.py
+++ 10/Term.py
@@ -1,6 +1,16 @@
-from Token import Token, LEXICAL_TYPE_SYMBOL, BRA_TOKEN, KET_TOKEN, SQUARE_BRA_TOKEN, SQUARE_KET_TOKEN
+from Token import Token, BRA_TOKEN, KET_TOKEN, SQUARE_BRA_TOKEN, SQUARE_KET_TOKEN
 
 UNARY_OPS = ["-", "~", "#", "^"]
+
+TERM_TYPE_INT = "integerConstant"
+TERM_TYPE_STRING = "stringConstant"
+TERM_TYPE_KEYWORD = "keywordConstant"
+TERM_TYPE_VAR = "varName"
+TERM_TYPE_VAR_W_EXP = "varName[expression]"
+TERM_TYPE_CALL = "subroutineCall"
+TERM_TYPE_EXP = "(expression)"
+TERM_TYPE_UNARY_OP = "unaryOp term"
+
 
 class Term:
     def __init__(self, tokens: list[Token]):
